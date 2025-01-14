885a9f45a999b2f577c67e1b163d95acb08ee67c
RoberMR <robermartinez777@gmail.com>
2025-01-14 21:23:03 +0100

    Añadido sonido al recoger el Medikit
    - Añadido un sonido para cuando recoges un Medikit.

--- Assets/PowerUps/Medikits/Medikit.cs
+++ Assets/PowerUps/Medikits/Medikit.cs
@@ -9,6 +9,7 @@ public class Medikit : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             other.gameObject.GetComponent<EntityLife>().AddHealthByMedikit(lifeAddedWhenPicked);
+            other.transform.GetChild(6).GetComponent<AudioSource>().Play();
             Destroy(gameObject);
         }
     }
